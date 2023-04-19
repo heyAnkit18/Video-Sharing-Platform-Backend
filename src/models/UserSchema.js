1acc05eeb39fea6fb0185c8fdbb2917c1ee110b4
gundlurubalaji <balajirock7093@gmail.com>
2023-04-19 12:32:51 +0530

    add schema

---
+++ src/models/UserSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    profession:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+
+    },
+    confirmpassword:{
+        type:String,
+        required:true
+
+    }
+})
+
+
+
+const User = mongoose.model("users",userSchema)
+module .exports= User
+
+

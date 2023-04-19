1d1f89fef168dc325c72e3f4af62901819a7b99f
Ankit <ankitkumarjune18@gmail.com>
2023-04-19 15:13:15 +0530

    added videoschema and videoupload

---
+++ src/Routes/Videos/VideoUpload.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("../database/connect.js");
+
+const VideoSchema = require("../models/VideoSchema.js");
+
+const router = express.Router();
+
+router.post("/VideoUpload", async (req, res) => {
+  const UserID = req.user._id;
+
+  const { Title, Describtion, ImageURL, VideoURL, Category, Visiblity } =
+    req.body;
+  const data = new VideoSchema({
+    UserID: UserID,
+    Title: Title,
+    Describtion: Describtion,
+    ImageURL: ImageURL,
+    VideoURL: VideoURL,
+    Category: Category,
+    Visiblity: Visiblity,
+  });
+  try {
+    const NewVideo = await data.save();
+    res.status(200).send("Congratulations! Video uploaded");
+  } catch (e) {
+    res.status(404).send("error");
+  }
+});

311b61e72df5f44edb6cb566535064ec148b817f
Amaanmohammad2000 <amaanmohammed2000@gmail.com>
2023-04-19 14:16:59 +0530

    index.js and connect.js files modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 const express = require("express")
 const app =express()
 const port =5000
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const mongoose = require("mongoose")
+require("./database/connect");
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
-
+// app.use("/", require("./Routes/Authentication/Register"));
+// app.use("/", require("./Routes/Authentication/Signin"));
+// app.use("/", require("./Routes/Videos/VideoSearch"));
+// app.use("/", require("./Routes/Videos/VideoUpload"));
 
 app.listen(port ,()=>{
-    console.log(`backend server running on ${port}`)
+    console.log(`backend server running on ${port}`);
 })

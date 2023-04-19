882f469b30c64c86436f0a8e5bad53d5ebe629b2
Ankit <ankitkumarjune18@gmail.com>
2023-04-19 12:10:48 +0530

    added to main

---
+++ src/database/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+
+const connect =()=>{
+ mongoose.connect(process.env.DATABASE).then(()=>{
+    console.log("database is connected")
+}).catch((e)=>{
+    console.log(e)
+})}
+connect()
--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ const app =express()
 const port =5000
 
 const mongoose = require("mongoose")
-const url ="mongodb+srv://ankitkumarjune18:videosharingproject@cluster0.s1bbyn2.mongodb.net/test"
 
-mongoose.connect(url).then(()=>{
-    console.log("database is connected")
-}).catch((e)=>{
-    console.log(e)
-})
+
 
 app.listen(port ,()=>{
     console.log(`backend server running on ${port}`)

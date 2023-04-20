04fa176af84087e444b91e6c73524e7454894440
Ankit <ankitkumarjune18@gmail.com>
2023-04-20 11:22:20 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app =express()
-const port =5000
+const port =8080
+const mongooos = require("mongoose")
 const cors = require("cors");
 const bodyParser = require("body-parser");
 

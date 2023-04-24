98779d5df0abdb38df1aa7c415c99c3c01954c44
Amaanmohammad2000 <amaanmohammed2000@gmail.com>
2023-04-24 16:45:55 +0530

    Upload.js, index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const PORT = 8080;
+
+dotenv.config();
+require("./db/conn.js")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
+
+
+app.use(require("./router/Upload.js"))
+app.use(require("./router/Search.js"))
+app.use(require("./router/Register.js"))
+app.use(require("./router/Signin.js"));
+
+
+const User=require("./model/usersSchema.js")
+const Video = require("./model/VideoSchema.js")
+
+
+
+const middleware=(req,res,next)=>{
+    console.log("i am midddleware")
+    next()
+}
+
+app.listen(PORT, (err) => {
+
+     if (err) console.log(err);
+    console.log(`Backend Server is running on Port ${PORT}`)
+})
+
